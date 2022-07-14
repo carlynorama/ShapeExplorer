0337010b40a36df46ed9f4ab323fe758cec5a52b
carlynorama <carlynorama@gmail.com>
2022-07-14 09:10:13 -0700

    Initial Commit

---
+++ ShapeExplorer/ShapeExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapeExplorerApp.swift
+//  ShapeExplorer
+//
+//  Created by Labtanza on 7/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ShapeExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
